f7eda6178310cf6df45ce0bc309c5b846b4bf65f
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 12:15:58 +0200

    Update do while.java

--- do while.java
+++ do while.java
@@ -1,10 +1,17 @@
+//Java program to Illustrate Do_while loop 
+//class
 class bello{
+    //Main driver method
 public static void main(String[] args) {
+    // Declare and initialize a variable
     int i=1;
+    //Do_while loop
     do{
+        //print statement
         System.out.println(i);
         i++;
     }
+        // checking condition
     while(i<=10);
 }
 }
