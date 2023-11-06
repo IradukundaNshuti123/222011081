fd7e5fda81c84493f6135cb5e4af6de784bb2e79
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 14:54:29 +0200

    Update while.java

--- while.java
+++ while.java
@@ -1,6 +1,9 @@
+// Java program to illustrate while loop.
 class ritt{
 public static void main(String[] args) {
+     // initialization expression
     int i=1;
+      // test expression
     while(i<6){
         System.out.println("kamila");
     }
