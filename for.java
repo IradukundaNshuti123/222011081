e7a461daa0377303137decb688fe63335b54f198
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 12:33:03 +0200

    Update for.java

--- for.java
+++ for.java
@@ -1,6 +1,9 @@
+// Java to print a text 5 times
 class kam {
     public static void main(String[] args) {
+        // for loop
         for(int i=1; i<=5; i++)
+            // print the statement 
     System.out.println("welcome");
 
     }
