5a175e0320204bc43b9b1d61b4f59afb9dfe1f4a
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 12:24:18 +0200

    Update else if.java

--- else if.java
+++ else if.java
@@ -1,11 +1,16 @@
+//Java program to illustrate if else
 class eee {
 public static void main(String[] args) {
+    //declare and initialise a variable
     int a=10;
     int b=15;
+    //checking if a is great than b
     if(a>b){
+        // print the out put
         System.out.println("a is great");
     }
     else{
+        // print the out put
         System.out.println("b is great");
     }
 }    
