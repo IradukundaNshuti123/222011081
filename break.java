0354b2c30fe45207e46d02f5d02665cd483a6683
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 11:22:51 +0200

    Update break.java

--- break.java
+++ break.java
@@ -1,13 +1,19 @@
- class huges 
+//use of break statement
+//inside the while loop
+class huges 
 {
 public static void main(String[] args) {
+ //creating and initializing variable
     int i=0;
+ //condition check
     while (i<20){
          if (i==6) {
+          //break statement
             break;
          }       
+     //printing element showcasing break
         System.out.println(i);
-        i++;
+        i++
     }
 }    
 }
