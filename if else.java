9364054f0fd7a5ebe4a09fab2151ee2a079f6394
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-06 12:41:39 +0200

    Update if else.java

--- if else.java
+++ if else.java
@@ -1,8 +1,11 @@
+//Java program to print a great number
 class dddd{
     public static void main(String[] args) {
+        //Declare and initialise a variable
         int a=10;
         int b=15;
         if(a>b);{
+            //print the out put
             System.out.println("a is great");
         }
     }
