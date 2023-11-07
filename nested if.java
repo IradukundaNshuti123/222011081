2ad21a67195916db670ca8bbd085fc76ba7b216a
IradukundaNshuti123 <149672734+IradukundaNshuti123@users.noreply.github.com>
2023-11-07 14:22:44 +0200

    Update nested if.java

---
+++ nested if.java
@@ -0,0 +1,20 @@
+// Java to demonstrate nested if
+public class ttt{
+  public static void main(string[] args){
+    int wages=100000;
+    if(wages<50000){
+      system.out.println("you're poor');
+    }
+    else if(wages>=60000){
+      system.out .println("you're in midder");
+        }
+    else if(wages=10000){
+      system.out.println("you're rich");
+        }
+    else if(wages>100000){
+      system.out.println("you're very rich");
+        }
+         else {
+           system.out.println("invalid");
+         }
+  }
